57a587f7fbdf8f09c4529da34f77ce3a99df3b21
Jordi Prats <jprats@systemadmin.es>
2016-05-23 15:53:48 +0200

    lint

--- manifests/includedir.pp
+++ manifests/includedir.pp
@@ -1,10 +1,10 @@
 define netbackupclient::includedir ($dir=$name) {
-	if ($::eyp_netbackupclient_version)
-	{
-		concat::fragment{ "include_$dir":
-			target  => '/usr/openv/netbackup/include_list',
-			content => "$dir\n",
-		}
-	}
+  if ($::eyp_netbackupclient_version)
+  {
+    concat::fragment{ "include_${dir}":
+      target  => '/usr/openv/netbackup/include_list',
+      content => "${dir}\n",
+    }
+  }
 
 }
